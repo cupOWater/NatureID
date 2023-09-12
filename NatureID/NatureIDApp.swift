3f8a6e1bff6c8d2615543166beb96f2cfc08d43b
cupOWater <something2805@gmail.com>
2023-09-12 10:37:52 +0700

    Initial Commit

---
+++ NatureID/NatureIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureIDApp.swift
+//  NatureID
+//
+//  Created by MacNCheese on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct NatureIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
